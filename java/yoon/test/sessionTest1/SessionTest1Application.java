1c370674e3bc9c777148b9f0b1577665a2275016
YoonSeungKwon <113295771+YoonSeungKwon@users.noreply.github.com>
2023-08-03 22:43:28 +0900

    Session Test

---
+++ java/yoon/test/sessionTest1/SessionTest1Application.java
@@ -0,0 +1,13 @@
+package yoon.test.sessionTest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionTest1Application.class, args);
+	}
+
+}
